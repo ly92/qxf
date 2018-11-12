01cf8d2ecf56dc48f47840733f8beb072c18a6b4
liyong <liyong@7xiaofu.com>
2018-11-12 17:57:38 +0800

    123

--- study/php/blog/resources/views/layouts/app.blade.php
+++ study/php/blog/resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 
     <title>@yield('title', config('app.name'))</title>
 
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="{{ mix('css/home.css') }}">
     <link rel="stylesheet" href="{{ mix('css/themes/' . config('blog.color_theme') . '.css') }}">
 
     <!-- Scripts -->
@@ -40,7 +40,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="js/home.js"></script>
+    <script src="{{ mix('js/home.js') }}"></script>
 
     @yield('scripts')
 
